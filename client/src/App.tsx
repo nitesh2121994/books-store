7bb1ba46d8090fe3405cd39e75a51400fde958b3
Nitesh Sharma <nitesh2121994@gmail.com>
2024-09-08 21:35:45 +0530

    Added book page, added tailwind css and request-query, updated cors policy on server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Route, Routes, Navigate  } from 'react-router-dom'
+import Home from './pages/Home'
+import UserProfile from './pages/UserProfile'
+import UserBookCollection from './pages/UserBookCollection'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<Navigate to="/home"/>}  />
+          <Route path='/home' element={<Home />} />
+          <Route path="/user-profile" element={<UserProfile />} />
+          <Route path="/your-collection" element={<UserBookCollection />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
